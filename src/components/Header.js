dafc6dd64efc5460874db32a994714b7100d81f3
Edgar Nyambo <edgarnyambo2@gmail.com>
2025-05-13 13:20:26 +0300

    uipdates

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,31 +11,38 @@ const activeClass = "block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-tra
 const inactiveClass = "block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
 
   return (
-    <header>   
-
-<nav className="bg-white border-gray-200 dark:bg-gray-900 border-b-4">
-    <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-        <Link to="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={Logo } className="h-12" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Shop</span>
+   <header>
+  <nav className="bg-white border-gray-200 dark:bg-gray-900 border-b-4">
+    <div className="max-w-screen-xl mx-auto p-4">
+      {/* Top section: Logo and Cart */}
+      <div className="flex flex-wrap items-center justify-between">
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={Logo} className="h-12" alt="Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Context-Shop</span>
         </Link>
-        <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
-            <p className=''>CartItems : {cartList.length}</p>
+        <div className="mt-4 md:mt-0">
+          <p className="text-black dark:text-white">CartItems: {cartList.length}</p>
         </div>
-        <div id="mega-menu" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-            <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
-                <li>
-                    <NavLink to="/" className={({ isActive }) => (isActive ? activeClass : inactiveClass)} end>Home</NavLink>
-                </li>
-              
-                <li>
-                    <NavLink to="/cart" className={({ isActive }) => (isActive ? activeClass : inactiveClass)}>Cart</NavLink>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+      </div>
 
-    </header>
+      {/* Bottom section: NavLinks */}
+      <div className="flex justify-center mt-4">
+        <ul className="flex flex-row flex-wrap justify-center gap-4">
+          <li>
+            <NavLink to="/" className={({ isActive }) => (isActive ? activeClass : inactiveClass)} end>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" className={({ isActive }) => (isActive ? activeClass : inactiveClass)}>
+              Cart
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+
   )
 }
